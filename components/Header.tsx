f051b6782d2760cedacfef2acc687c69bcdd220f
asheeshkundara <asheeshkundara@gmail.com>
2026-05-14 13:05:10 +0530

    change
    change contact us

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ export default function Header() {
           <li><Link href="/system-integration">Our Results</Link></li>
           <li><Link href="/custom-website-developement">Insight</Link></li>
           <button className="px-3 py-1 text-black rounded bg-[#EEF430]">
-            <Link href={'/custom-app-developement'}>Contact Us</Link>
+            <Link href={'/custom-app-developement'}>Contact us</Link>
           </button>
         </ul>
       </nav>
